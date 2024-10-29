1321bc9e362e2dd892e3bf4f4d8b486b07653564
t200056 <gimpo.t200056@ggh.goe.go.kr>
2024-10-29 12:55:14 +0900

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -1,19 +1,18 @@
 # streamlit 라이브러리 불러오기
 import streamlit as st
 
-st.title('양수 음수 판별기')  # 제목 쓰기
+st.title('동생아 수학 공부하자!')  # 제목 쓰기
 
-st.subheader('양수음수 판별기') # 부제목 쓰기
+st.subheader('양수와 음수') # 부제목 쓰기
 
 st.write('음수를 아시나요?')                  # 본문 쓰기
 
 col1, col2 = st.columns(2)       # 여러 개의 열(문단)을 생성
 with col1:
-       st.write('0보다 큰 수는 양수')
+       st.write('양수: 0보다 큰 수')
+       st.write('음수: 0보다 작은 수, (-)부호가 붙음')
 with col2:
-       st.write('0보다 작은 수는 음수')
-
-st.image( 'chart (1).png' )              # 이미지 불러오기
+       st.image( 'chart (1).png' )              # 이미지 불러오기
 
 a = st.number_input('수를 입력하세요', min_value=0)   # 사용자의 입력을 받아서 a에 저장하기(최소값은 0)
 
