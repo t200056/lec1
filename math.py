af959025bab309e14200b63ae8547f304ff68766
t200056 <gimpo.t200056@ggh.goe.go.kr>
2024-10-29 14:21:07 +0900

    Update math.py

--- math.py
+++ math.py
@@ -11,24 +11,24 @@ st.write('음수?? ______않아!')
 # 여러 개의 열(문단)을 생성
 col1, col2 = st.columns(2)       
 # 왼쪽 문단
-with col1:
+#with col1:
        st.subheader('**개념정리')
        st.write('- 양수: 0보다 큰 수')
        st.write('- 음수: 0보다 작은 수')
        st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
        st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
 # 오른쪽 문단
-with col2:
+#with col2:
 #      st.image('______________')  # 이미지 파일 불러오기
 
 # 사용자의 입력을 받아서 a에 저장하기(초기값은 0)
 #a = st.number_input('____________', value= ____)  
 
 # 버튼 생성 및 동작
-if st.button("양수일까 ____일까?"):
+if st.button('양수일까 ____일까?'):
        if a > 0:
               st.write('________')
-       elif a<0:
+       elif a < 0:
               st.write('________')
        else:
               st.write('________')
